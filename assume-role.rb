7080b452703e3b18822fb96ec0ecd201e05d27e7
graham jenson <graham.jenson@coinbase.com>
2017-10-19 10:46:15 +1300

    update version for PR

--- assume-role.rb
+++ assume-role.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AssumeRole < Formula
   homepage 'https://github.com/coinbase/assume-role'
-  url 'https://github.com/coinbase/assume-role/archive/v0.1.1.tar.gz'
-  sha256 '8ad1f8541196c9fe87f3c1dc5458fc8e8c0c605713d8952ccab86575f386a24b'
+  url 'https://github.com/coinbase/assume-role/archive/v0.1.2.tar.gz'
+  sha256 '3d8b4577e7baecd508066d39d2019c8f0451a8d36c6d743e5982e131b0bdad96'
 
   def install
     bin.install File.join('.', 'assume-role')
