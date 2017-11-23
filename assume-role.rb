6981121a30a1f0de4342cb4944fa6d4f995dc548
Marco Bonetti <sid77@users.noreply.github.com>
2017-11-23 12:11:45 +0000

    Add dependencies

--- assume-role.rb
+++ assume-role.rb
@@ -5,6 +5,9 @@ class AssumeRole < Formula
   url 'https://github.com/coinbase/assume-role/archive/v0.1.3.tar.gz'
   sha256 '6c214b17408c705e7a897247eb4e054587b5e4785e76541dc981024c04da7bd8'
 
+  depends_on "awscli"
+  depends_on "jq"
+
   def install
     bin.install File.join('.', 'assume-role')
   end
